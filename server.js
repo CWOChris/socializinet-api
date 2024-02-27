df83e36e04fe1ec5845e309b4a120127ff7dad90
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-02-26 19:18:39 -0600

    building server.js

---
+++ server.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const apiRouter = require('./routes/thought-routes');
+const apiRouter2 = require('./routes/user-routes.js');
+
+const 
